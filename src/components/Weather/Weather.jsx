07a3eab950b454a01f6dde765cb3e23c918b265f
Gosia Hildebrand <ras.malgorzata@gmail.com>
2023-10-19 17:24:20 +0200

    Fetching weather data from API and displaying it inside component

--- src/components/Weather/Weather.jsx
+++ src/components/Weather/Weather.jsx
@@ -1,14 +1,30 @@
 import axios from "axios";
 import React, { useEffect, useState } from "react";
+import { useGeolocated } from "react-geolocated";
 
 const Weather = () => {
-  const [weather, setWeather] = useState("");
+  const { coords } = useGeolocated({
+    positionOptions: {
+      enableHighAccuracy: false,
+    },
+    userDecisionTimeout: 5000,
+  });
+
+  const [weather, setWeather] = useState(null);
 
   useEffect(() => {
-    async function fetchWeather() {
+    async function fetchWeatherDaily() {
+      if (
+        !coords ||
+        typeof coords.longitude !== "number" ||
+        typeof coords.latitude !== "number"
+      ) {
+        return;
+      }
+
       try {
         const response = await axios.get(
-          "https://api.open-meteo.com/v1/forecast?latitude=52.52&longitude=13.41&daily=temperature_2m_max,temperature_2m_min,sunrise,sunset,precipitation_sum,precipitation_probability_max,windspeed_10m_max&timezone=Europe%2FBerlin"
+          `https://api.open-meteo.com/v1/forecast?latitude=${coords.latitude}&longitude=${coords.longitude}&daily=temperature_2m_max,precipitation_probability_max,precipitation_sum,windspeed_10m_max&timezone=Europe%2FBerlin`
         );
         setWeather(response.data);
         console.log(response.data);
@@ -17,81 +33,87 @@ const Weather = () => {
       }
     }
 
-    console.log(fetchWeather());
-  }, []);
+    fetchWeatherDaily();
+  }, [coords]);
   return (
-    <div className="w-full justify-center container mx-auto weather">
-      <div className="flex flex-wrap w-full lg:w-auto">
-        <div className="w-full lg:w-1/2 flex rounded-lg bg-auto">
-          <div className="rounded-lg py-6 pl-8 pr-32 w-full text-zinc-200">
-            <div className="mb-20">
-              <h2 className="font-bold text-3xl leading-none pb-1">Tuesday</h2>
-              <h3 className="leading-none pb-2 pl-1">15 Jan 19</h3>
+    <section className="flex flex-wrap w-full justify-center container mx-auto weather">
+      {weather && (
+        <>
+          <article className="w-full lg:w-1/2 flex rounded-lg bg-auto">
+            <div className="rounded-lg p-8 w-full text-zinc-200">
+              <header className="mb-20">
+                <h2 className="font-bold text-3xl leading-none pb-1">
+                  {new Date(weather.daily.time[0]).toLocaleString("en-US", {
+                    weekday: "long",
+                  })}
+                </h2>
+                <time className="leading-none pb-2 pl-1">
+                  {new Date(weather.daily.time[0]).toLocaleDateString()}
+                </time>
+              </header>
+              <div>
+                <strong className="leading-none text-6xl block font-weight-bolder">
+                  {weather.daily.temperature_2m_max[0]}ºC
+                </strong>
+              </div>
             </div>
-            <div>
-              <strong className="leading-none text-6xl block font-weight-bolder">
-                29ºC
-              </strong>
-              <b className="text-2xl block font-bold">Sunny</b>
-            </div>
-          </div>
-        </div>
+          </article>
 
-        <div className="w-full lg:w-1/2 flex ml-0">
-          <div className="lg:my-3 text-zinc-200 text-lg p-4 lg:rounded-r-lg w-full ">
-            <div className="flex justify-between mb-4 w-full">
-              <div className="w-auto font-bold uppercase text-90">
-                Precipitation
-              </div>
-              <div className="w-auto text-right">10 %</div>
-            </div>
-            <div className="flex justify-between mb-4 w-full">
-              <div className="w-auto font-bold uppercase text-90">Humidity</div>
-              <div className="w-auto text-right">29 %</div>
-            </div>
-            <div className="flex justify-between mb-8 w-full">
-              <div className="w-auto font-bold uppercase text-90">Wind</div>
-              <div className="w-auto text-right">12 Mph</div>
-            </div>
-            <div className="flex flex-row gap-2">
-              <div className="flex flex-col w-1/4 bg-gray-100 text-black rounded-lg pb-4">
-                <div className="text-center pt-2 mb-2"></div>
-                <div className="text-center">
-                  <b className="font-normal">Tue</b>
-                  <br />
-                  <strong className="text-xl">29ºC</strong>
+          <aside className="w-full lg:w-1/2 flex ml-0">
+            <div className="lg:my-3 text-zinc-200 text-lg p-4 lg:rounded-r-lg w-full">
+              <dl>
+                <div className="flex justify-between mb-4 w-full">
+                  <dt className="w-auto font-bold uppercase text-90">
+                    Precipitation
+                  </dt>
+                  <dd className="w-auto text-right">
+                    {weather.daily.precipitation_probability_max[0]} %
+                  </dd>
                 </div>
-              </div>
+                <div className="flex justify-between mb-4 w-full">
+                  <dt className="w-auto font-bold uppercase text-90">
+                    Precipitation Sum
+                  </dt>
+                  <dd className="w-auto text-right">
+                    {weather.daily.precipitation_sum[0]}mm
+                  </dd>
+                </div>
+                <div className="flex justify-between mb-4 w-full">
+                  <dt className="w-auto font-bold uppercase text-90">
+                    Wind Speed
+                  </dt>
+                  <dd className="w-auto text-right">
+                    {weather.daily.windspeed_10m_max[0]}km/h
+                  </dd>
+                </div>
+              </dl>
 
-              <div className="flex flex-col w-1/4 bg-gray-900 rounded-lg">
-                <div className="text-center pt-2 mb-2"></div>
-                <div className="text-center">
-                  <b className="font-normal">Wed</b>
-                  <br />
-                  <strong className="text-xl">21ºC</strong>
-                </div>
-              </div>
-              <div className="flex flex-col w-1/4 bg-gray-900 rounded-lg">
-                <div className="text-center pt-2 mb-2"></div>
-                <div className="text-center">
-                  <b className="font-normal">Thu</b>
-                  <br />
-                  <strong className="text-xl">18ºC</strong>
-                </div>
-              </div>
-              <div className="flex flex-col w-1/4 bg-gray-900 rounded-lg">
-                <div className="text-center pt-2 mb-2"></div>
-                <div className="text-center">
-                  <b className="font-normal">Fri</b>
-                  <br />
-                  <strong className="text-xl">25ºC</strong>
-                </div>
+              <div className="flex flex-row gap-2">
+                {weather.daily.time.slice(1, 5).map((date, index) => (
+                  <div
+                    key={date}
+                    className="flex flex-col w-1/4 bg-gray-900 rounded-lg"
+                  >
+                    <div className="text-center pt-2 mb-2"></div>
+                    <div className="text-center">
+                      <b className="font-normal">
+                        {new Date(date).toLocaleString("en-US", {
+                          weekday: "short",
+                        })}
+                      </b>
+                      <br />
+                      <strong className="text-xl">
+                        {weather.daily.temperature_2m_max[index]}ºC
+                      </strong>
+                    </div>
+                  </div>
+                ))}
               </div>
             </div>
-          </div>
-        </div>
-      </div>
-    </div>
+          </aside>
+        </>
+      )}
+    </section>
   );
 };
 
--- src/layouts/Header.js
+++ src/layouts/Header.js
@@ -6,12 +6,6 @@ import React from "react";
 import { Disclosure } from "@headlessui/react";
 import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/outline";
 
-const user = {
-  name: "Tom Cook",
-  email: "tom@example.com",
-  imageUrl:
-    "https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80",
-};
 const navigation = [
   { name: "Dashboard", href: "#", current: true },
   { name: "Weather", href: "#", current: false },
