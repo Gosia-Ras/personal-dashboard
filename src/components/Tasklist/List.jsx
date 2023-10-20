cc376c01b2ac2b401b8bf15fa232d83c9bec62c9
Gosia Hildebrand <ras.malgorzata@gmail.com>
2023-10-20 18:59:29 +0200

    Added task counter to the Tasklist

--- src/components/Tasklist/List.jsx
+++ src/components/Tasklist/List.jsx
@@ -3,9 +3,26 @@ import { ListItem } from "./ListItem";
 export function List({ todos, toggleTodo, deleteTodo }) {
   return (
     <ul className="list">
-      {todos.length === 0 && (
-        <p className="text-sm">There's nothing on your list yet :)</p>
-      )}
+      {(() => {
+        if (todos.length === 0) {
+          return (
+            <p className="text-sm mb-5">There's nothing on your list yet :)</p>
+          );
+        } else if (todos.length === 1) {
+          return (
+            <p className="text-sm mb-5">
+              You have {todos.length} task to complete
+            </p>
+          );
+        } else {
+          return (
+            <p className="text-sm mb-5">
+              You have {todos.length} tasks to complete
+            </p>
+          );
+        }
+      })()}
+
       {todos.map((todo) => {
         return (
           <ListItem
