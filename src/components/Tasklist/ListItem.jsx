cc11893ccca6b969df84b24eecc7d7f426885b8f
Gosia Hildebrand <ras.malgorzata@gmail.com>
2023-10-21 13:41:15 +0200

    Changed icons for completed tasks to FontAwesome icons

--- src/components/Tasklist/ListItem.jsx
+++ src/components/Tasklist/ListItem.jsx
@@ -1,17 +1,20 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCircle, faCheckCircle } from "@fortawesome/free-solid-svg-icons";
+
 export function ListItem({ completed, title, id, toggleTodo, deleteTodo }) {
   return (
-    <li key={id} className="mb-3">
-      <label htmlFor="" className="mr-3 text-xl">
-        <input
-          className="mr-3"
-          type="checkbox"
-          checked={completed}
-          onChange={(e) => toggleTodo(id, e.target.checked)}
+    <li key={id} className="mb-3 py-3 flex flex-row justify-between item">
+      <label htmlFor="" className="mr-3 text-xl flex items-center">
+        <FontAwesomeIcon
+          icon={completed ? faCheckCircle : faCircle}
+          className={completed ? "text-green-500 mr-3" : "text-gray-500 mr-3"}
+          onClick={() => toggleTodo(id, !completed)}
+          style={{ cursor: "pointer" }}
         />
-        {title}
+        <span className={completed ? "line-through" : ""}>{title}</span>
       </label>
       <button
-        className="bg-rose-800 text-white px-4 py-2 rounded hover:bg-rose-600 transition-all duration-200 "
+        className="bg-rose-800 text-white px-4 py-2 rounded hover:bg-rose-600 transition-all duration-300"
         onClick={() => deleteTodo(id)}
       >
         Delete
